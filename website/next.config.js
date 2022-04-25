fa88d119c66f53bdd6dd10f1f985f865a657482c
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-04-26 01:26:26 +0200

    Accepting svg images from trusted domains

--- website/next.config.js
+++ website/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	images: { domains: ["cdn.sanity.io"] },
+	images: {
+		domains: ["cdn.sanity.io"],
+		dangerouslyAllowSVG: true,
+		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+	},
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;

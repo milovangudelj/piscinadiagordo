2484fe61c863b0f8ae6fee3e9c93f0c22058b614
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-04-26 01:27:10 +0200

    Implemented recent news feed

--- website/components/RecentNews/RecentNews.tsx
+++ website/components/RecentNews/RecentNews.tsx
@@ -1,5 +1,36 @@
+import { format } from "date-fns";
+import Image from "next/image";
+import Link from "next/link";
+import { urlForImage } from "../../lib/sanity";
+import { Button } from "../Button";
 import { Container } from "../Container";
 
-export const RecentNews = () => {
-	return <Container>Recent News</Container>;
+export const RecentNews = ({ posts }: { posts: any[] }) => {
+	return (
+		<Container className="my-12">
+			<h2>News</h2>
+			<ol className="mt-12 flex gap-12">
+				{posts.map((post) => (
+					<li key={post._id}>
+						<div className="relative h-[200px] w-[380px] overflow-hidden rounded-lg">
+							<Image
+								src={urlForImage(post.coverImage).quality(100).url()}
+								layout="fill"
+								objectFit="cover"
+								alt={post.coverImage.alt}
+							/>
+						</div>
+						<h3 className="mt-6 text-h4">{post.title}</h3>
+						<span className="mt-2 mb-4 inline-block text-caption text-dark-me">
+							{format(new Date(post.publishedAt), "d LLL, yyyy")}
+						</span>
+						<p className="mb-6 text-base">{post.excerpt}</p>
+						<Button as={Link} href={`/news/${post.slug}`} passHref>
+							Leggi tutto
+						</Button>
+					</li>
+				))}
+			</ol>
+		</Container>
+	);
 };

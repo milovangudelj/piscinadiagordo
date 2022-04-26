a8900644c619b2f67a6f88f19b11c8f809e77bf9
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-04-26 19:20:18 +0200

    Switched from post header to page header

--- website/components/Header/Header.tsx
+++ website/components/Header/Header.tsx
@@ -1,12 +1,33 @@
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
+import { ReactNode } from "react";
+import { Container } from "../Container";
 
-export const Header = () => {
+export const Header = ({
+	title,
+	image,
+	imageAlt,
+	children,
+}: {
+	title: string;
+	image: string | StaticImageData;
+	imageAlt: string;
+	children: ReactNode;
+}) => {
 	return (
-		<h2 className="mb-20 mt-8 text-2xl font-bold leading-tight tracking-tight md:text-4xl md:tracking-tighter">
-			<Link href="/" passHref>
-				<a className="hover:underline">Blog</a>
-			</Link>
-			.
-		</h2>
+		<header>
+			<Container className="pb-12">
+				<div className="relative mb-12 h-[400px]">
+					<Image
+						src={image}
+						layout="fill"
+						objectFit="cover"
+						alt={imageAlt}
+					/>
+				</div>
+				<h1 className="mb-4">{title}</h1>
+				<div className="text-base">{children}</div>
+			</Container>
+		</header>
 	);
 };

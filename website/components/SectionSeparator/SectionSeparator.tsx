31534f2187775943786a3caf219f5960afbffbcf
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-04-26 19:22:12 +0200

    Added support for `className` prop

--- website/components/SectionSeparator/SectionSeparator.tsx
+++ website/components/SectionSeparator/SectionSeparator.tsx
@@ -1,8 +1,8 @@
 import { Container } from "../Container";
 
-export const SectionSeparator = () => {
+export const SectionSeparator = ({ className }: { className?: string }) => {
 	return (
-		<Container>
+		<Container className={className}>
 			<hr className="border-gray-100" />
 		</Container>
 	);

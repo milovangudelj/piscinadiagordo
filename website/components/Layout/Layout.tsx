fd134652409afc56e787a8d4fba802b5eb8412fe
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-04-26 19:20:36 +0200

    Removed preview prop

--- website/components/Layout/Layout.tsx
+++ website/components/Layout/Layout.tsx
@@ -1,19 +1,12 @@
 import { Alert, Footer, Meta } from "../";
 import { Navbar } from "../Navbar";
 
-export const Layout = ({
-	preview,
-	children,
-}: {
-	preview: boolean;
-	children: any;
-}) => {
+export const Layout = ({ children }: { children: any }) => {
 	return (
 		<>
 			<Meta />
 			<Navbar />
 			<div className="min-h-screen">
-				{/* <Alert preview={preview} /> */}
 				<main>{children}</main>
 			</div>
 			<Footer />

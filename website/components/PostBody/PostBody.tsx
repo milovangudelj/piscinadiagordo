3a8f3f7fdb8e542c7cfe87ad71dc349ba9950092
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-04-29 22:43:21 +0200

    Restyled component slightly

--- website/components/PostBody/PostBody.tsx
+++ website/components/PostBody/PostBody.tsx
@@ -1,9 +1,10 @@
-import markdownStyles from "./markdown-styles.module.css";
 import { PortableText } from "@portabletext/react";
+
+import markdownStyles from "./markdown-styles.module.css";
 
 export const PostBody = ({ content }: { content: any }) => {
 	return (
-		<div className={`mx-auto max-w-2xl ${markdownStyles.markdown}`}>
+		<div className={`mx-auto max-w-prose ${markdownStyles.markdown}`}>
 			<PortableText value={content} />
 		</div>
 	);

d7b10aed4114a6b771f360931b6df5f1c4e5f837
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-04-29 23:04:29 +0200

    Added `@svgr/webpack` for svg component imports

--- website/next.config.js
+++ website/next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
 		dangerouslyAllowSVG: true,
 		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ["@svgr/webpack"],
+		});
+
+		return config;
+	},
 };
 
 module.exports = nextConfig;

25fb19d5e536f45c54216fb380ffb0f9579afb9a
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-05-03 13:09:51 +0200

    Removed unnecessary fragment

--- website/components/AttivitaPalazzetto/AttivitaPalazzetto.tsx
+++ website/components/AttivitaPalazzetto/AttivitaPalazzetto.tsx
@@ -13,22 +13,20 @@ export const AttivitaPalazzetto = ({
 	children: ReactNode;
 }) => {
 	return (
-		<>
-			<div className="flex">
-				<div className="relative h-[400px] w-[300px]">
-					<Image
-						src={image}
-						placeholder="blur"
-						layout="fill"
-						objectFit="cover"
-						alt={imageAlt}
-					/>
-				</div>
-				<div className="ml-12 flex-1">
-					<h3>{title}</h3>
-					<div className="mt-4 space-y-4 text-base">{children}</div>
-				</div>
+		<div className="flex">
+			<div className="relative h-[400px] w-[300px]">
+				<Image
+					src={image}
+					placeholder="blur"
+					layout="fill"
+					objectFit="cover"
+					alt={imageAlt}
+				/>
 			</div>
-		</>
+			<div className="ml-12 flex-1">
+				<h3>{title}</h3>
+				<div className="mt-4 space-y-4 text-base">{children}</div>
+			</div>
+		</div>
 	);
 };

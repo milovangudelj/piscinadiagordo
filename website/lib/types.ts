22d31fe6a164c5322ee22cd58afffb3c6567df05
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-05-04 17:01:46 +0200

    `RequireOnlyOne` type

--- website/lib/types.ts
+++ website/lib/types.ts
@@ -1,3 +1,12 @@
+export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
+	T,
+	Exclude<keyof T, Keys>
+> &
+	{
+		[K in Keys]-?: Required<Pick<T, K>> &
+			Partial<Record<Exclude<Keys, K>, undefined>>;
+	}[Keys];
+
 export type Person = {
 	_id: string;
 	name: string;
@@ -19,12 +28,12 @@ export type Post = {
 export type Frequenza = {
 	_id: string;
 	name: string;
-}
+};
 
 export type Corso = {
 	_id: string;
 	title: string;
 	description: any;
-	cover:any;
+	cover: any;
 	frequenza: Frequenza[];
-}
+};

ba4a6c0e3e4d5b9307b7909559f9bcf811e81456
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-05-04 17:02:45 +0200

    Table specific to pricing data

---
+++ website/components/PricingTable/PricingTable.tsx
@@ -0,0 +1,42 @@
+import classNames from "classnames";
+
+// Components
+import { Table, Chip, PricingLabel } from "../";
+
+type PricingItem = {
+	label: string;
+	details?: string;
+	value: string;
+};
+
+type PricingTableProps = {
+	title: string;
+	prices: PricingItem[];
+	className?: string;
+};
+
+export const PricingTable = ({
+	title,
+	prices,
+	className,
+}: PricingTableProps) => {
+	return (
+		<Table
+			columns={[
+				{ header: title, className: "w-full" },
+				{ header: "Tariffa" },
+			]}
+			rows={prices.map((price) => [
+				{
+					content: (
+						<PricingLabel label={price.label} details={price.details} />
+					),
+				},
+				{
+					content: <Chip type="success">{price.value}</Chip>,
+				},
+			])}
+			className={classNames("table-auto", className)}
+		/>
+	);
+};

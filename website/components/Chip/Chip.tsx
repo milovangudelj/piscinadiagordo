9da24b959c1f90ba7611192e3b893fcf56191810
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-05-17 20:35:45 +0200

    Complete implementation of `Chip` component

--- website/components/Chip/Chip.tsx
+++ website/components/Chip/Chip.tsx
@@ -1,39 +1,143 @@
 import classNames from "classnames";
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode, useEffect, useState } from "react";
 
-type ChipType = {
-	warning: string;
-	error: string;
-	info: string;
+type ChipColor = {
+	primary: string;
+	secondary: string;
 	success: string;
+	error: string;
 };
 
-export const Chip = ({
-	type = "info",
-	children,
-}: {
-	type?: keyof ChipType;
+type ChipSize = {
+	sm: string;
+	md: string;
+	lg: string;
+};
+
+type ChipVariant = {
+	filled: string;
+	outlined: string;
+	borderless: string;
+};
+
+const classes = {
+	base: (clickable: boolean) =>
+		classNames(
+			"inline-block h-min rounded-full font-medium transition py-0",
+			{ "cursor-pointer": clickable },
+			{ "cursor-default": !clickable }
+		),
+	variant: {
+		filled: "text-light-he",
+		outlined: "border bg-transparent",
+		borderless: "bg-transparent",
+	},
+	colors: (clickable: boolean) => ({
+		filled: {
+			primary: classNames("bg-primary-500", {
+				"hover:bg-primary-400 active:bg-primary-600": clickable,
+			}),
+			secondary: classNames("bg-secondary-500", {
+				"hover:bg-secondary-400 active:bg-secondary-600": clickable,
+			}),
+			success: classNames("bg-success-500", {
+				"hover:bg-success-400 active:bg-success-600": clickable,
+			}),
+			error: classNames("bg-error-500", {
+				"hover:bg-error-400 active:bg-error-600": clickable,
+			}),
+			disabled: classNames("bg-gray-500", {
+				"hover:bg-gray-400 active:bg-gray-600": clickable,
+			}),
+		},
+		outlined: {
+			primary: classNames("border-primary-500 text-primary-500", {
+				"hover:bg-primary-500/5 active:bg-primary-500/10": clickable,
+			}),
+			secondary: classNames("border-secondary-500 text-secondary-500", {
+				"hover:bg-secondary-500/5 active:bg-secondary-500/10": clickable,
+			}),
+			success: classNames("border-success-500 text-success-500", {
+				"hover:bg-success-500/5 active:bg-success-500/10": clickable,
+			}),
+			error: classNames("border-error-500 text-error-500", {
+				"hover:bg-error-500/5 active:bg-error-500/10": clickable,
+			}),
+			disabled: classNames("border-gray-500 text-gray-500", {
+				"hover:bg-gray-500/5 active:bg-gray-500/10": clickable,
+			}),
+		},
+		borderless: {
+			primary: classNames("text-primary-500", {
+				"hover:bg-primary-500/5 active:bg-primary-500/10": clickable,
+			}),
+			secondary: classNames("text-secondary-500", {
+				"hover:bg-secondary-500/5 active:bg-secondary-500/10": clickable,
+			}),
+			success: classNames("text-success-500", {
+				"hover:bg-success-500/5 active:bg-success-500/10": clickable,
+			}),
+			error: classNames("text-error-500", {
+				"hover:bg-error-500/5 active:bg-error-500/10": clickable,
+			}),
+			disabled: classNames("text-gray-500", {
+				"hover:bg-gray-500/5 active:bg-gray-500/10": clickable,
+			}),
+		},
+	}),
+	sizes: {
+		sm: {
+			padding: "px-1",
+			text: "text-button-xs",
+		},
+		md: {
+			padding: "px-2",
+			text: "text-button-sm",
+		},
+		lg: {
+			padding: "px-3",
+			text: "text-button",
+		},
+	},
+};
+
+interface ChipProps {
+	color?: keyof ChipColor;
+	size?: keyof ChipSize;
+	variant?: keyof ChipVariant;
+	disabled?: boolean;
+	onClick?: MouseEventHandler<HTMLSpanElement>;
 	children: ReactNode;
-}) => {
+}
+
+export const Chip = ({
+	color = "primary",
+	size = "md",
+	variant = "filled",
+	disabled = false,
+	onClick,
+	children,
+}: ChipProps) => {
+	const makeStyles = (): string => {
+		return classNames(
+			classes.base(onClick !== undefined && !disabled),
+			classes.variant[variant],
+			classes.colors(onClick !== undefined && !disabled)[variant][
+				!disabled ? color : "disabled"
+			],
+			classes.sizes[size].padding,
+			classes.sizes[size].text
+		);
+	};
+
+	const [styles, setStyles] = useState(makeStyles);
+
+	useEffect(() => {
+		setStyles(makeStyles);
+	}, [color, size, variant, makeStyles]);
+
 	return (
-		<span
-			className={classNames(
-				`inline-block rounded-full border px-2 py-0.5 text-base leading-none`,
-				{
-					"border-secondary-500 bg-secondary-50 text-secondary-500":
-						type === "warning",
-				},
-				{ "border-error-500 bg-error-50 text-error-500": type === "error" },
-				{
-					"border-primary-500 bg-primary-50 text-primary-500":
-						type === "info",
-				},
-				{
-					"border-success-500 bg-success-50 text-success-500":
-						type === "success",
-				}
-			)}
-		>
+		<span onClick={disabled ? undefined : onClick} className={styles}>
 			{children}
 		</span>
 	);

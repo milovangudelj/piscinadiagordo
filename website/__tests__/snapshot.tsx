7df65b2972ecc515dbed69585fa42a6765a7e6e5
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-05-22 18:55:08 +0200

    Useless rn

--- website/__tests__/snapshot.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import Home from "../pages/index";
-
-it("renders homepage unchanged", () => {
-	const result: any = render(<Home sponsors={[]} posts={[]} />);
-	expect(result.container).toMatchSnapshot();
-});

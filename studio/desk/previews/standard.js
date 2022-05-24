8fc9bb3a37c1f39ea037bb3c8eb21d7f53e16ce6
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-05-24 15:40:36 +0200

    Formatter + Changed title

--- studio/desk/previews/standard.js
+++ studio/desk/previews/standard.js
@@ -6,9 +6,6 @@ import EditIcon from 'part:@sanity/base/edit-icon'
 import SeoPreview from './seo/seo-preview'
 
 export const standardViews = [
-  S.view.form().icon(EditIcon),
-  S.view
-    .component(SeoPreview)
-    .icon(EyeIcon)
-    .title('SEO Preview')
-]
+	S.view.form().icon(EditIcon),
+	S.view.component(SeoPreview).icon(EyeIcon).title("Anteprima SEO"),
+];
